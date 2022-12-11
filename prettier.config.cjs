73f0fe9a91328abbb4b258fea81bf1522d827c02
sinsky <60662802+sinsky@users.noreply.github.com>
2022-12-12 01:20:57 +0900

    update prettier format rule

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
+  importOrderSortSpecifiers: true,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
