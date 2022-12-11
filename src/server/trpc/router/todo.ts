1bf96a81f2944aa76313991486ab42af3119fe67
sinsky <60662802+sinsky@users.noreply.github.com>
2022-12-12 01:09:34 +0900

    add todo router

---
+++ src/server/trpc/router/todo.ts
@@ -0,0 +1,69 @@
+import {
+  createTaskSchema,
+  getSingleTaskSchema,
+  updateTaskSchema,
+  deleteTaskSchema,
+} from "schema/todo";
+import { router, publicProcedure, protectedProcedure } from "server/trpc/trpc";
+
+export const todoRouter = router({
+  // protectedProcedure --> 認証が必要
+  createTask: protectedProcedure
+    .input(createTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+      return task;
+    }),
+  // publicProcedure --> 認証が不要
+  getTasks: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.task.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }),
+  getSingleTask: protectedProcedure
+    .input(getSingleTaskSchema)
+    .query(({ ctx, input }) => {
+      return ctx.prisma.task.findUnique({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+  updateTask: protectedProcedure
+    .input(updateTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.update({
+        where: {
+          id: input.taskId,
+        },
+        data: {
+          title: input.title,
+          body: input.body,
+        },
+      });
+      return task;
+    }),
+  deleteTask: protectedProcedure
+    .input(deleteTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.task.delete({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+});
