301f3ee2e04f0f5b1ae785a760e46974c3985403
sinsky <60662802+sinsky@users.noreply.github.com>
2022-12-14 00:47:51 +0900

    enabled line provider

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,12 +2,18 @@ import { signIn } from "next-auth/react";
 
 const Auth = () => {
   return (
-    <div>
+    <div className="flex flex-col [&>*]:my-4">
       <button
         className="rounded bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800"
         onClick={() => signIn("github")}
       >
         GitHub Auth
+      </button>
+      <button
+        className="rounded bg-green-600 py-2 px-4 font-bold text-white hover:bg-green-800"
+        onClick={() => signIn("line")}
+      >
+        LINE Auth
       </button>
     </div>
   );
